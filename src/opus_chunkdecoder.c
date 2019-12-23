61f48ba493b7206858e1967a8b922b1bc450aa97
Chris McGowan <chris@anthum.com>
2019-12-22 22:40:18 -0500

    bump versionto1.2.4

--- src/opus_chunkdecoder.c
+++ src/opus_chunkdecoder.c
@@ -1,7 +1,7 @@
 #include "opus_chunkdecoder.h"
 
 char* opus_chunkdecoder_version() {
-  return "opus chunkdecoder 1.2";
+  return "opus chunkdecoder 1.2.4";
 }
 
 static int cb_read(OpusChunkDecoder *decoder, unsigned char *_ptr, int _nbytes) {
